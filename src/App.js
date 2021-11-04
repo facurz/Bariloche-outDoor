2533a85157ab6f3b60f1a9f82e4d6239a1b65aa3
Facundo Riva Zabala <facurz@hotmail.com>
2021-11-04 11:07:36 -0300

    instalacion react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from './components/NavBar'
 
 function App() {
   return (
-    <div>
+    <>
     <NavBar/>
-    </div>
+    </>
 
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,50 +1,50 @@
 
 export const NavBar = () => {
     return(
-        <div>
-            <header>
         
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark pt-2 pb-2">
-                    <div className="container d-flex justify-content-lg-around">
-                    <a className="navbar-brand" href="index.html"><img width="100" height="100" src="https://barilocheoutdoor.com/wp-content/uploads/2021/07/logo.webp" alt="Bariloche Outdoor logo" loading="lazy"/></a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto ms-auto mb-2 mb-lg-0 botones">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="index.html">Inicio</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="index.html" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Categorías
-                            </a>
-                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a className="dropdown-item" href="index.html">Camping</a></li>
-                            <li><a className="dropdown-item" href="index.html">Montaña</a></li>
-                            <li><a className="dropdown-item" href="index.html">Kayaks</a></li>
-                            <li><a className="dropdown-item" href="index.html">Bicis</a></li>
-                            <li><a className="dropdown-item" href="index.html">Ski</a></li>
-                            </ul>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="index.html">Carrito</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="index.html">Contacto</a>
-                        </li>
-                        
+        <header>
+    
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark pt-2 pb-2">
+                <div className="container d-flex justify-content-lg-around">
+                <a className="navbar-brand" href="index.html"><img width="100" height="100" src="https://barilocheoutdoor.com/wp-content/uploads/2021/07/logo.webp" alt="Bariloche Outdoor logo" loading="lazy"/></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto ms-auto mb-2 mb-lg-0 botones">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="index.html">Inicio</a>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="index.html" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Categorías
+                        </a>
+                        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a className="dropdown-item" href="index.html">Camping</a></li>
+                        <li><a className="dropdown-item" href="index.html">Montaña</a></li>
+                        <li><a className="dropdown-item" href="index.html">Kayaks</a></li>
+                        <li><a className="dropdown-item" href="index.html">Bicis</a></li>
+                        <li><a className="dropdown-item" href="index.html">Ski</a></li>
                         </ul>
-                        <form className="d-flex">
-                        <input className="form-control me-2 formBuscar" type="search" placeholder="Buscar" aria-label="Search"/>
-                        <button className="btn btn-outline-light formBuscar" type="submit">Buscar</button>
-                        </form>
-                    </div>
-                    </div>
-                </nav>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="index.html">Carrito</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="index.html">Contacto</a>
+                    </li>
+                    
+                    </ul>
+                    <form className="d-flex">
+                    <input className="form-control me-2 formBuscar" type="search" placeholder="Buscar" aria-label="Search"/>
+                    <button className="btn btn-outline-light formBuscar" type="submit">Buscar</button>
+                    </form>
+                </div>
+                </div>
+            </nav>
 
-            </header>
-        </div>
+        </header>
+        
     )
 
 }
